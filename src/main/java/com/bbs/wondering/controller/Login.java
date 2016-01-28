13574983c4f284d37eee8bb4d58809891c551211
yuetiansanshao <53710908@qq.com>
2016-01-28 13:12:35 +0800

    更换配置文件

--- src/main/java/com/bbs/wondering/controller/Login.java
+++ src/main/java/com/bbs/wondering/controller/Login.java
@@ -1,7 +1,9 @@
 package com.bbs.wondering.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpRequest;
@@ -10,9 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.support.HttpRequestHandlerServlet;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bbs.wondering.model.User;
+import com.bbs.wondering.service.TestService;
+
 @Controller
 @RequestMapping("/main")
 public class Login {
+	//@Resource
+	private TestService testService = new TestService();
 	
 	@RequestMapping("/login.html")
 	public String login(HttpServletRequest re){
@@ -25,6 +32,12 @@ public class Login {
 		for(String key : map.keySet() ){
 			System.out.println("key :"+key+", value :"+map.get(key));
 		}
+
+		List<User> list = testService.findAll();
+		for(User user :list){
+			System.out.println(user.getName());
+		}
+
 		return "welcome";
 	}
 /*	
--- src/main/java/com/bbs/wondering/service/TestService.java
+++ src/main/java/com/bbs/wondering/service/TestService.java
@@ -1,13 +1,50 @@
 package com.bbs.wondering.service;
 
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.orm.hibernate4.HibernateCallback;
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
-public class TestService {
-	
-	public String test(){
-		String hql = "select * from User";
-	//Query query = session.createQuery(hql);
-		return hql;
+import com.bbs.wondering.model.User;
+
+public class TestService extends HibernateDaoSupport{
+	public List<User> findAll(){
+		final String hql = "select t from User";
+/*		SessionFactory sessionFactory = getSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Query query =	session.createQuery(hql);
+		List list = query.list();
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+		return list;*/
+
+/*		HibernateTemplate hibernateTemplate = getHibernateTemplate();
+		//hibernateTemplate.setCacheQueries(false);
+		return (List<User>)hibernateTemplate.find(hql);*/
+		
+		
+	//	sql = 
+				
+		List<User> list= (List)super.getHibernateTemplate().execute(new HibernateCallback<Object>(){
+			@Override
+			public Object doInHibernate(Session session) throws HibernateException {
+				// TODO Auto-generated method stub
+				Query query = session.createQuery(hql);
+				query.setCacheable(false);
+				List list = query.list();
+				session.close();
+				return list;
+			}
+		});
+		return list;
 	}
 }
