3be0bbbdbb7d7e3da2c7d7372e468d76381cde57
yuetiansanshao <53710908@qq.com>
2016-03-01 16:16:59 +0800

    增加了上传控件

--- src/main/java/com/bbs/wondering/controller/FileUploadCotroller.java
+++ src/main/java/com/bbs/wondering/controller/FileUploadCotroller.java
@@ -4,12 +4,16 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller
 @RequestMapping("/main")
@@ -22,13 +26,16 @@ public class FileUploadCotroller {
     }
 
     @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam(value="name" , required = false) String name,
-            @RequestParam(value="file",required = false) MultipartFile file){
+    public @ResponseBody String handleFileUpload(HttpServletRequest request/*,
+            @RequestParam(value="fileupload",required = false) MultipartFile file*/){
+    	 MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+         MultipartFile file = multipartRequest.getFile("Filedata");
+         String name= file.getOriginalFilename();
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
-                BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                File newFile = new File("D:\\apache-tomcat-7.0.32\\work\\Catalina\\localhost\\upload",name);
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(newFile));
                 stream.write(bytes);
                 stream.close();
                 return "You successfully uploaded " + name + "!";
